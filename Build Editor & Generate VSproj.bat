f76b23ed7c031d58e4c20de8e408a75ea4cc995e
pupil1337 <2500698263@qq.com>
2023-09-27 22:04:23 +0800

    自定义模块编译失败修复 原因:SCsub非.Clone()

--- Build Editor & Generate VSproj.bat
+++ Build Editor & Generate VSproj.bat
@@ -1,4 +1,3 @@
 cd godot
-scons compiledb=yes compile_commands.json
 scons platform=windows bits=64 target=editor custom_modules=../ppgame dev_build=yes debug_symbols=yes vsproj=yes
 pause
---
+++ ppgame/src/player/fsm/ground/playerRunState.cpp
@@ -0,0 +1,1 @@
+#include "playerRunState.h"
