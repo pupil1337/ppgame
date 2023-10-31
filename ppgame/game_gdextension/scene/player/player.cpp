0cc0c1a6e687a9d515671a16f68a886c7e092278
pupil1337 <2500698263@qq.com>
2023-10-31 18:32:30 +0800

    修改工作区配置

--- ppgame/game_gdextension/scene/player/player.cpp
+++ ppgame/game_gdextension/scene/player/player.cpp
@@ -1,8 +1,14 @@
 #include "player.h"
 
+#include <godot_cpp/classes/engine.hpp>
+
 void Playertest::_bind_methods() {
 }
 
 void Playertest::_ready() {
 	CharacterBody2D::_ready();
+
+	if (!Engine::get_singleton()->is_editor_hint()) {
+		ERR_FAIL_EDMSG("这是个测试用222");
+	}
 }
