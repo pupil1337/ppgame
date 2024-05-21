f9b67af8dee93e45979ba7db8cf460d773666db3
pupil1337 <2500698263@qq.com>
2024-05-22 02:02:21 +0800

    vscode插件 启动参数可配置

--- ppgame/game_gdextension/gm/gm.cpp
+++ ppgame/game_gdextension/gm/gm.cpp
@@ -2,14 +2,6 @@
 
 #include <thirdpart/imgui/imgui.h>
 #include <thirdpart/imgui/imgui_internal.h>
-#include <godot_cpp/classes/canvas_item.hpp>
-#include <godot_cpp/classes/collision_shape2d.hpp>
-#include <godot_cpp/classes/engine.hpp>
-#include <godot_cpp/classes/node.hpp>
-#include <godot_cpp/classes/object.hpp>
-#include <godot_cpp/classes/scene_tree.hpp>
-#include <godot_cpp/classes/window.hpp>
-#include <godot_cpp/variant/typed_array.hpp>
 
 GM* GM::singleton = nullptr;
 
@@ -56,21 +48,6 @@ void GM::_flush_impl() {
 		ImGui::Checkbox("Demo Window", &show_demo_window);
 
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-
-		// - show debug collision
-		if (SceneTree* tree = Object::cast_to<SceneTree>(Engine::get_singleton()->get_main_loop())) {
-			bool show_debug_collision = tree->is_debugging_collisions_hint();
-			if (ImGui::Checkbox("show_debug_collision", &show_debug_collision)) {
-				tree->set_debug_collisions_hint(show_debug_collision);
-
-				TypedArray<Node> nodes = tree->get_root()->find_children({}, Node::get_class_static(), true, false);
-				for (int i = 0; i < nodes.size(); ++i) {
-					if (CollisionShape2D* shape = Object::cast_to<CollisionShape2D>(nodes[i])) {
-						shape->propagate_notification(CanvasItem::NOTIFICATION_DRAW);
-					}
-				}
-			}
-		}
 
 		ImGui::End();
 	}
